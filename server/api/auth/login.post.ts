74568e958f43858c723f70251797eef7a58cdc46
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-07-02 16:47:25 +0700

    fix : server.login :: add try catch

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,35 +1,51 @@
 import bcrypt from 'bcryptjs';
+import { createRefreshToken } from '~/server/services/refreshToken.service';
 import { getUserByUsername } from '~/server/services/user.service';
-import { throwBadRequest } from '~/server/utils/error';
-import { generateTokens } from '~/server/utils/jwt';
+import { userTransform } from '~/server/transforms/user.transform';
+import { throwBadRequest, throwInternalServerError } from '~/server/utils/error';
+import { generateTokens, sendRefreshToken } from '~/server/utils/jwt';
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  const { username, password } = body;
+  try {
+    const body = await readBody(event);
+    const { username, password } = body;
 
-  if (!username || !password) {
-    return throwBadRequest(event, 'Invaild params');
+    if (!username || !password) {
+      return throwBadRequest(event, 'Invalid params');
+    }
+
+    // Is the user registered
+    const user = await getUserByUsername(username);
+    if (!user) {
+      return throwBadRequest(event, 'Username or password is invalid');
+    }
+
+    // Compare passwords
+    const doesThePasswordMatch = await bcrypt.compare(password, user.password);
+    if (!doesThePasswordMatch) {
+      return throwBadRequest(event, 'Username or password is invalid');
+    }
+
+    // Generate token
+    const { accessToken, refreshToken } = generateTokens(user);
+
+    // Save RefreshToken
+    await createRefreshToken({
+      token: refreshToken,
+      userId: user.id,
+    });
+
+    // Add http only cookie
+    sendRefreshToken(event, refreshToken);
+
+    return {
+      data: {
+        access_token: accessToken,
+        user: userTransform(user),
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return throwInternalServerError(event);
   }
-
-  // Is the user registered
-  const user = await getUserByUsername(username);
-  if (!user) {
-    return throwBadRequest(event, 'Username or password is invaild');
-  }
-
-  // Compare passwords
-  const doesThePasswordMatch = await bcrypt.compare(password, user.password);
-  if (!doesThePasswordMatch) {
-    return throwBadRequest(event, 'Username or password is invaild');
-  }
-
-  // Generate token
-  const { accessToken, refreshToken } = generateTokens(user);
-
-  return {
-    data: {
-      accessToken,
-      refreshToken,
-    },
-  };
 });
