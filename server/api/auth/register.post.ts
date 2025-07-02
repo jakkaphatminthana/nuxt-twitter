19a993ed3aac9813b97d42f1cad82fa449f2a123
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-07-02 16:57:57 +0700

    feat : server.register :: add try catch

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -4,48 +4,52 @@ import { createUser, getUserByUsername } from '~/server/services/user.service';
 import { userTransform } from '~/server/transforms/user.transform';
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  try {
+    const body = await readBody(event);
+    const { username, email, password, repeatPassword, name } = body;
 
-  const { username, email, password, repeatPassword, name } = body;
+    // Validate required fields
+    if (!username || !email || !password || !repeatPassword || !name) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: 'Missing required fields',
+        }),
+      );
+    }
 
-  // Validate required fields
-  if (!username || !email || !password || !repeatPassword || !name) {
-    return sendError(
-      event,
-      createError({
-        statusCode: 400,
-        statusMessage: 'Missing required fields',
-      }),
-    );
+    // Validate password match
+    if (password !== repeatPassword) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: 'Passwords do not match',
+        }),
+      );
+    }
+
+    // Validate has alreay user
+    const existsUser = await getUserByUsername(username);
+    if (!!existsUser) {
+      return throwBadRequest(event, 'User already exists');
+    }
+
+    const userData: Prisma.UserCreateInput = {
+      username,
+      email,
+      password,
+      name,
+    };
+
+    const user = await createUser(userData);
+
+    return {
+      data: userTransform(user),
+    };
+  } catch (error) {
+    console.error(error);
+    return throwInternalServerError(event);
   }
-
-  // Validate password match
-  if (password !== repeatPassword) {
-    return sendError(
-      event,
-      createError({
-        statusCode: 400,
-        statusMessage: 'Passwords do not match',
-      }),
-    );
-  }
-
-  // Validate has alreay user
-  const existsUser = await getUserByUsername(username);
-  if (!!existsUser) {
-    return throwBadRequest(event, 'User already exists');
-  }
-
-  const userData: Prisma.UserCreateInput = {
-    username,
-    email,
-    password,
-    name,
-  };
-
-  const user = await createUser(userData);
-
-  return {
-    data: userTransform(user),
-  };
 });
